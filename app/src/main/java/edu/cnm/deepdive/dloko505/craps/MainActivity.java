4228ce6219f921b3f39b78ba4cdfb491c3e6a0ec
Dloko505 <Dloko505@gmail.com>
2018-06-08 11:37:26 -0600

    Initial commit of craps on android.

---
+++ app/src/main/java/edu/cnm/deepdive/dloko505/craps/MainActivity.java
@@ -0,0 +1,140 @@
+package edu.cnm.deepdive.dloko505.craps;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+import edu.cnm.deepdive.craps.Game;
+import edu.cnm.deepdive.craps.Roll;
+import edu.cnm.deepdive.craps.State;
+import java.security.SecureRandom;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+  private Game game;
+  private Button play;
+  private Button reset;
+  private ToggleButton run;
+  private ListView rolls;
+  private TextView tally;
+  private Random rng;
+  private RollsAdaptor rollsAdaptor;
+  private long plays = 0;
+  private long wins = 0;
+  private boolean running = false;
+  private Worker worker = null;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    play = findViewById(R.id.play_game);
+    reset = findViewById(R.id.reset_game);
+    run = findViewById(R.id.play_continuously);
+    rolls = findViewById(R.id.game_rolls);
+    tally = findViewById(R.id.tally);
+    game = new Game();
+    rng = new SecureRandom();
+    rollsAdaptor = new RollsAdaptor(this, R.layout.item_roll);
+    rolls.setAdapter(rollsAdaptor);
+    reset();
+    play.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        play();
+        updateDisplay(game.getState(), game.getRolls(), plays ,wins);
+      }
+    });
+    reset.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        reset();
+      }
+    });
+    run.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+      @Override
+      public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        toggleRun(isChecked);
+      }
+    });
+  }
+
+  private void toggleRun(boolean isChecked) {
+    if (isChecked) {
+      play.setEnabled(false);
+      reset.setEnabled(false);
+      worker = new Worker();
+      running = true;
+      worker.start();
+    } else {
+      running = false;
+      worker = null;
+      reset.setEnabled(true);
+      play.setEnabled(true);
+    }
+  }
+
+  private void reset() {
+    plays = 0;
+    wins = 0;
+    updateDisplay(game.getState(), null, plays, wins);
+  }
+
+  private void play() {
+    game.reset();
+    game.play(rng);
+    plays++;
+    if (game.getState() ==State.WIN) {
+      wins++;
+    }
+  }
+
+  private void updateDisplay(State state, Roll[] rolls, long plays, long wins) {
+    double percentage = (plays > 0) ? (100d * wins / plays) : 0;
+    tally.setText(getString(R.string.tally_pattern,
+        wins, plays, percentage));
+    rollsAdaptor.clear();
+    rollsAdaptor.setState(state);
+    if (rolls != null) {
+      rollsAdaptor.addAll(rolls);
+    }
+  }
+
+  private class Worker extends Thread {
+
+    private edu.cnm.deepdive.craps.State state;
+    private Roll[] rolls;
+    private long threadPlays;
+    private long threadWins;
+
+    @Override
+    public void run() {
+      while (running) {
+        do {
+          play();
+        } while (plays % 100 != 0);
+        state = game.getState();
+        rolls = game.getRolls();
+        threadPlays = plays;
+        threadWins = wins;
+        runOnUiThread(new Runnable() {
+          @Override
+          public void run() {
+            updateDisplay(state, rolls, threadPlays, threadWins);
+          }
+        });
+      }
+    }
+  }
+}
